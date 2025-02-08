621f838316a006747b00430acce12646e5ba3f5c
Harpreet Singh <01sharpreet@gmail.com>
2025-02-08 16:30:29 +0530

    Login Page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-	return <div>Login</div>;
+  const [userName, setUserName] = useState("elon@gmail.com");
+  const [password, setPassword] = useState("Elon@123");
+
+  const handleLogin = async () => {
+    try {
+      await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId: userName,
+          password: password,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <label className="input validator mt-2">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </g>
+            </svg>
+            <input
+              type="input"
+              required
+              placeholder="Username"
+              pattern="[A-Za-z][A-Za-z0-9\-]*"
+              minLength="3"
+              maxLength="30"
+              title="Only letters, numbers or dash"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </label>
+          <p className="validator-hint -mt-2">
+            Must be 3 to 30 characters
+            <br />
+            containing only letters, numbers or dash
+          </p>
+
+          <label className="input validator ">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              minLength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <p className="validator-hint hidden ">
+            Must be more than 8 characters, including
+            <br />
+            At least one number
+            <br />
+            At least one lowercase letter
+            <br />
+            At least one uppercase letter
+          </p>
+          <div className="card-actions justify-center my-3">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
